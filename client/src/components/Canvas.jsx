5a0958574994f7e49a08964cc88c43179c88b1d1
William <william@lancehub.com>
2017-05-22 20:56:10 +0800

    add export

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { toJS } from 'mobx';
 import { inject } from 'mobx-react';
 import Node from './Node';
+import '../styles/canvas.less';
 
 @inject('tree')
 class Canvas extends React.PureComponent {
   render() {
     return (
-      <div className="canvas">
-        <Node tree={this.props.tree} />
+      <div className="container">
+        <div className="canvas">
+          <Node tree={this.props.tree} />
+        </div>
+        <div className="footer">
+          <a onClick={() => console.log(toJS(this.props.tree))}>Export</a>
+        </div>
       </div>
     );
   }
